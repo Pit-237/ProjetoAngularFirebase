b13adb562e150ce55aafb588da0acfeb89ecd743
Pit <parkergamer237@gmail.com>
2026-02-26 21:22:55 +0000

    layout

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -12,17 +12,11 @@ import { Router } from '@angular/router';
 })
 export class HomePage {
 
-  pokemon:any = {
-    nome: null,
-    poder: null
-  };
-
-  constructor( 
-    public crudService: CrudService
-  ){ }
-
-  enviar() {
-    this.crudService.insert(this.pokemon, 'pokemons');
-  }
+  projeto: any[] = [
+    { descricao: "tomar banho", local: "casa", hora_inicio: "19:00", hora_termino: "19:30", status: "concluido" },
+    { descricao: "não morrer de fome", local: "qualquer um", hora_inicio: "00:00", hora_termino: "23:59", status: "em progresso" },
+    { descricao: "matar jogadores de LOL", local: "escola", hora_inicio: "13:10", hora_termino: "18:30", status: "em espera" },
+    { descricao: "ir à academia", local: "academia", hora_inicio: "19:10", hora_termino: "21:30", status: "em espera" }
+  ];
 
 }
